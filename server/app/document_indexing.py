4d9109b3531e93093f807eb6892fda5bdd557f31
John Maher <johnapmaher@gmail.com>
2024-11-04 15:30:09 +0000

    Enhance document indexing by adding S3 integration and text processing

--- server/app/document_indexing.py
+++ server/app/document_indexing.py
@@ -1,7 +1,9 @@
 import boto3
 from aws_lambda_powertools import Logger
+from langchain_community.document_loaders import TextLoader
 from langchain_community.vectorstores import  OpenSearchVectorSearch
 from langchain_community.embeddings import OpenAIEmbeddings
+from langchain_text_splitters import CharacterTextSplitter
 from dotenv import load_dotenv
 import os
 
@@ -10,12 +12,19 @@ load_dotenv()
 s3 = boto3.client('s3')
 logger = Logger()
 
+bucket_name = os.environ.get("S3_BUCKET_NAME")
+
 def read_document_from_s3(bucket_name, document_key):
     response = s3.get_object(Bucket=bucket_name, Key=document_key)
     document_content = response['Body'].read().decode('utf-8')
     return document_content
 
 def index_document(document_content):
+    loader = TextLoader(document_content)
+    
+    documents = loader.load()
+    text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
+    docs = text_splitter.split_documents(documents)
     # Initialize OpenAI embeddings
     embeddings = OpenAIEmbeddings(api_key=os.getenv('OPENAI_API_KEY'))
 
@@ -32,6 +41,16 @@ def index_document(document_content):
 @logger.inject_lambda_context
 def handler(event, context):
     logger.info(f"Received event: {event}")
+    
+    for record in event['Records']:
+        if record['eventName'] == 'INSERT':
+            new_image = record['dynamodb']['NewImage']
+            files_list = new_image.get('files', {}).get('L', [])
 
-    document_content = read_document_from_s3(bucket_name, document_key)
-    index_document(document_content)
+            for file_entry in files_list:
+                document_key = file_entry.get('S')
+                if document_key:
+                    document_content = read_document_from_s3(bucket_name, document_key)
+                    index_document(document_content)
+                else:
+                    logger.error('No document key found in file entry.')
